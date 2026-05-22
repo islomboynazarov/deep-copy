a12954f9da1557c849f9d2f163f130b753f8defd
islomboynazarov <islom.boynazarov@gmail.com>
2026-05-22 16:15:54 +0500

    feat: add recursive copying for arrays

--- deep-copy/index.js
+++ deep-copy/index.js
@@ -10,6 +10,14 @@ function copy(value) {
   if (value instanceof Date) {
     return new Date(value.getTime());
   }
+
+  if (Array.isArray(value)) {
+    var arrCopy = [];
+    for (var i = 0; i < value.length; i += 1) {
+      arrCopy[i] = copy(value[i]);
+    }
+    return arrCopy;
+  }
 }
 
 module.exports = { copy };
